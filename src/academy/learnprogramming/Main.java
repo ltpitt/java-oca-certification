e2a4e1cbcf6471cbbaeee3aa8efc55d848c30aff
Davide Nastri <d.nastri@gmail.com>
2019-09-07 09:50:45 +0200

    feat: Main method

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -1,4 +1,29 @@
 package academy.learnprogramming;
 
 public class Main {
+
+
+
+
+    public static void main(String[] args){
+        System.out.println("arg-size=" + args.length);
+
+        for(int i = 0; i < args.length; i++) {
+            System.out.println("args[" + i + "]=" + args[i]);
+        }
+
+    }
+
+    /**
+     * Calculates sum of two integers.
+     *
+     * @param a operand
+     * @param b operand
+     * @return sum
+     */
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+
 }
