9b562b66949213ac316d80cd336deee26f1fa3df
Davide Nastri <d.nastri@gmail.com>
2019-09-12 09:27:54 +0200

    feat: Import Example

---
+++ src/academy/learnprogramming/ImportExample.java
@@ -0,0 +1,12 @@
+package academy.learnprogramming;
+
+import java.util.Random;
+
+public class ImportExample {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        System.out.println(random.nextInt(5));
+    }
+
+}
