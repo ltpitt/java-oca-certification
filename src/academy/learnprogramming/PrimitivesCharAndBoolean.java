be88eb52a94ecdb5740ba5c80785d5265059316a
Davide Nastri <d.nastri@gmail.com>
2019-09-20 20:14:21 +0200

    Implemented PrimitivesCharAndBoolean

---
+++ src/academy/learnprogramming/PrimitivesCharAndBoolean.java
@@ -0,0 +1,26 @@
+package academy.learnprogramming;
+
+
+public class PrimitivesCharAndBoolean {
+
+    public static void main(String[] args) {
+        char ch = 'a';
+
+        // char ch1 = 'ab';
+        char ch1 = '1';
+        char uniChar = '\u03A9'; // upper case greek omega character
+        char romanNumber = '\u216C'; // roman 50 number
+
+        System.out.println("ch1= " + ch1);
+        System.out.println("uniChar= " + uniChar);
+        System.out.println("romanNumber= " + romanNumber);
+
+        boolean myBoolean = true;
+        boolean myFalseBoolean = false;
+
+        System.out.println("myBoolean= " + myBoolean);
+        System.out.println("myFalseBoolean= " + myFalseBoolean);
+
+        boolean true1 = false;
+    }
+}
