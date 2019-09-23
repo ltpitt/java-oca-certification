c2e4a398fbfa745d00a2fe5f4e55c2b9515b10d8
Davide Nastri <d.nastri@gmail.com>
2019-09-23 21:15:50 +0200

    Implemented DefaultInitialization

---
+++ src/academy/learnprogramming/DefaultInitialization.java
@@ -0,0 +1,28 @@
+package academy.learnprogramming;
+
+public class DefaultInitialization {
+
+    static boolean myBoolean;
+    static byte myByte;
+    static short myShort;
+    static int myInt;
+    static long myLong;
+    static float myFloat;
+    static double myDouble;
+    static char myChar;
+
+    public static void main(String[] args) {
+        int localInt;
+
+//        System.out.println("localInt= " + localInt);
+
+        System.out.println("myBoolean= " + myBoolean);
+        System.out.println("myByte= " + myByte);
+        System.out.println("myShort= " + myShort);
+        System.out.println("myInt= " + myInt);
+        System.out.println("myLong= " + myLong);
+        System.out.println("myFloat= " + myFloat);
+        System.out.println("myDouble= " + myDouble);
+        System.out.println("myChar= " + myChar);
+    }
+}
