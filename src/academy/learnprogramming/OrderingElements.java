57b1c3b1e4b5d1649e0409a7a8253fde1fdce43f
Davide Nastri <d.nastri@gmail.com>
2019-09-26 22:33:37 +0200

    Implemented OrderingElements

---
+++ src/academy/learnprogramming/OrderingElements.java
@@ -0,0 +1,30 @@
+//import java.util.Random;
+
+package academy.learnprogramming;
+
+//package academy;
+
+//int myInt = 10;
+
+//public void myMethod() {}
+
+class MyOtherClass {
+}
+
+class AnotherClass {
+}
+
+public class OrderingElements {
+
+    int myInt = 10;
+
+    public static void main(String[] args) {
+        int anotherInt = 20;
+
+//        void myMethod() {
+//
+//        }
+    }
+
+    // PIC (package, import, class)
+}
