e90a0276546e61a4025cbbf1db9502c2ed5e945e
Davide Nastri <d.nastri@gmail.com>
2019-10-24 21:16:04 +0200

    Implemented StringMethods2

---
+++ src/academy/learnprogramming/StringMethods2.java
@@ -0,0 +1,41 @@
+package academy.learnprogramming;
+
+/**
+ * @author goran on 6/07/2017.
+ */
+public class StringMethods2 {
+
+    public static void main(String[] args) {
+        String str = "java";
+        String str2 = "Java";
+        String str3 = "ja";
+
+        System.out.println(str.equals(str2));
+        System.out.println(str.equalsIgnoreCase(str2));
+        System.out.println("abc".equalsIgnoreCase("ABC")); // true
+
+        // startsWith / endsWith
+        System.out.println(str.startsWith("j"));
+        System.out.println(str.startsWith(str3));
+        System.out.println(str.startsWith("J".toLowerCase()));
+        System.out.println(str.endsWith("va"));
+
+        // contains
+        System.out.println(str.contains(str3));
+        System.out.println(str.contains("av"));
+        System.out.println("Java".contains("j".toUpperCase()));
+
+        // replace
+        String myString = "Java" + " " + "is" + " " + "cool";
+
+        System.out.println(myString.replace('a', 'A'));
+        System.out.println(myString.replace(" ", "_"));
+        System.out.println(myString.replace("is", "_"));
+        System.out.println(myString);
+
+        // trim
+        System.out.println("Java".trim());
+        System.out.println(" Java is cool  ");
+        System.out.println(" Java is cool  ".trim());
+    }
+}
