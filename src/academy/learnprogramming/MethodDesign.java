23e2974081f7140749849e6270c2a8811db238c3
Davide Nastri <d.nastri@gmail.com>
2019-11-20 22:16:53 +0100

    Implemented MethodDesign

---
+++ src/academy/learnprogramming/MethodDesign.java
@@ -0,0 +1,28 @@
+package academy.learnprogramming;
+
+public class MethodDesign {
+
+    public static final void jump5() {
+    }
+
+//    void public jump2() {} // does not compile
+
+    public final static void jump6() {
+    }
+
+    //    public array void jump7() {}
+//    public String int float double jump8() {}
+//    final public void final jump9() {}
+    final static public void jump10() {
+    }
+
+    public void jump() {
+    }
+
+    void jump3() {
+    }
+
+    public final void jump4() {
+    }
+//    String int public void jump11() {} // does not compile
+}
