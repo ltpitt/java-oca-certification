eef3c2fab766ecf50bd96550bab37b6a0a1ca6bf
Davide Nastri <d.nastri@gmail.com>
2020-03-09 22:30:22 +0100

    Solved Question_1

---
+++ src/academy/learnprogramming/Questions/Question_1.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming.Questions;
+
+public class Question_1 {
+
+    public static void main(String[] args) {
+        System.out.println("5 + 2 = " + 3 + 4);
+        System.out.println("5 + 2 = " + (3 + 4));
+    }
+
+    // Prints 34 and 7
+
+}
+
+
