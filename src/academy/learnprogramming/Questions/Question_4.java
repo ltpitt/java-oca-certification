d0aebd3f162b44e6faab488e10cd2b6548af1c9b
Davide Nastri <d.nastri@gmail.com>
2020-03-12 20:12:56 +0100

    Solved Question_4

---
+++ src/academy/learnprogramming/Questions/Question_4.java
@@ -0,0 +1,14 @@
+package academy.learnprogramming.Questions;
+
+public class Question_4 {
+
+    public static void main(String[] args) {
+        String[] vargs = {"Hello"};
+        if (vargs[0].equals("Hello") ? true : false) {
+            System.out.println("Success");
+        } else {
+            System.out.println("Failure");
+        }
+    }
+}
+
