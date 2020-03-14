ac4ad0e0ecb40e04e2e095c1bdf8649ad702ee6f
Davide Nastri <d.nastri@gmail.com>
2020-03-14 18:06:14 +0100

    Solved Question_6

---
+++ src/academy/learnprogramming/Questions/Question_6.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming.Questions;
+
+public class Question_6 {
+
+    public static void main(String[] args) {
+        int ii = 0;
+        int jj = 7;
+        for (ii = 0; ii < jj; ii = ii + 2) {
+            System.out.println(ii + " ");
+        }
+    }
+
+}
