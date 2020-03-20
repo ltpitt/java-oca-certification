3d2a03ebecefccc7ac3be757438b21e3943cf30f
Davide Nastri <d.nastri@gmail.com>
2020-03-20 23:13:05 +0100

    Solved Question 7

---
+++ src/academy/learnprogramming/Questions/Question_7.java
@@ -0,0 +1,23 @@
+package academy.learnprogramming.Questions;
+
+public class Question_7 {
+}
+
+abstract class Planet {
+    protected void revolve() {
+
+    }
+
+    abstract void rotate();
+}
+
+class Earth extends Planet {
+    // Make this method public or protected
+//    void revolve() {
+//
+//    }
+
+    protected void rotate() {
+
+    }
+}
