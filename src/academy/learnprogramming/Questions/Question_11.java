6555de87cef13f9babdfdd566a60b795d998fb81
Davide Nastri <d.nastri@gmail.com>
2020-03-21 22:24:25 +0100

    Solved Question 11

---
+++ src/academy/learnprogramming/Questions/Question_11.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming.Questions;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Question_11 {
+
+    public static void main(String[] args) {
+        String date = LocalDate.parse("2014-05-04").format(DateTimeFormatter.ISO_DATE_TIME);
+        System.out.println(date);
+    }
+
+}
