72d5f5259d80cc0f12548f7177d1bdbf8673b382
Davide Nastri <d.nastri@gmail.com>
2020-03-24 22:54:32 +0100

    Solved Question 13

---
+++ src/academy/learnprogramming/Questions/Question_13.java
@@ -0,0 +1,21 @@
+package academy.learnprogramming.Questions;
+
+public class Question_13 {
+
+    public static void main(String[] args) {
+
+        // All three work perfectly
+
+        byte x = 1;
+//        short x = 1;
+//        Integer x = new Integer(1);
+
+        switch (x) {
+            case 1:
+                break;
+            case 2:
+                break;
+        }
+    }
+
+}
