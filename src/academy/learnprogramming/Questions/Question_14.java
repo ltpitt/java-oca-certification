57116bcc227fb7db4b467c995dfe733f3e631cbd
Davide Nastri <d.nastri@gmail.com>
2020-03-26 20:13:42 +0100

    Solved Question 14

---
+++ src/academy/learnprogramming/Questions/Question_14.java
@@ -0,0 +1,8 @@
+package academy.learnprogramming.Questions;
+
+public class Question_14 {
+
+    public static void main(String[] args) {
+        System.out.printf("String main " + args[0]);
+    }
+}
