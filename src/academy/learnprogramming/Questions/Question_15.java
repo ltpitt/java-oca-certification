b282da62b81585ef56460f576a938467fa609f29
Davide Nastri <d.nastri@gmail.com>
2020-03-28 19:36:10 +0100

    Solved Question 15

---
+++ src/academy/learnprogramming/Questions/Question_15.java
@@ -0,0 +1,17 @@
+package academy.learnprogramming.Questions;
+
+public class Question_15 {
+}
+
+class Testo {
+    public static void main(String[] args) {
+        int numbers[] = {12, 13, 41, 32, 15, 156, 23, 51, 12};
+        int[] keys = findMax(numbers);
+    }
+
+    // Compiles perfectly
+    static int[] findMax(int[] max) {
+        int[] keys = new int[3];
+        return keys;
+    }
+}
