adbe245eb4a00cdcc645960453d7ddc0ee24857a
Davide Nastri <d.nastri@gmail.com>
2020-03-29 22:52:38 +0200

    Solved Question 16

---
+++ src/academy/learnprogramming/Questions/Question_16.java
@@ -0,0 +1,19 @@
+package academy.learnprogramming.Questions;
+
+public class Question_16 {
+
+    public static void main(String[] args) {
+        String[][] arr = {{"A", "B", "C"}, {"D", "E"}};
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.println(arr[i][j] + " ");
+                if (arr[i][j].equals("B")) {
+                    continue;
+                }
+            }
+            continue;
+
+        }
+    }
+
+}
