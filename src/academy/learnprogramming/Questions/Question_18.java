0e96fa2f0e9f0ce2202c4c20e792151175535c58
Davide Nastri <d.nastri@gmail.com>
2020-03-31 23:04:54 +0200

    Solved Question 18

---
+++ src/academy/learnprogramming/Questions/Question_18.java
@@ -0,0 +1,12 @@
+package academy.learnprogramming.Questions;
+
+public class Question_18 {
+
+    public static void main(String[] args) {
+        boolean a = new Boolean(Boolean.valueOf(args[0]));
+        boolean b = new Boolean(args[1]);
+        System.out.println(a + " " + b);
+    }
+
+}
+
