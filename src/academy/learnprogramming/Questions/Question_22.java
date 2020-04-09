531e7deb78b397adb7d0c789c2d749715ce15f3c
Davide Nastri <d.nastri@gmail.com>
2020-04-10 00:18:57 +0200

    Solved Question 22

---
+++ src/academy/learnprogramming/Questions/Question_22.java
@@ -0,0 +1,12 @@
+package academy.learnprogramming.Questions;
+
+public class Question_22 {
+
+    public static void main(String[] args) {
+        String[] arr = {"Hi", "How", "Are", "You"};
+        // Doesn't compile
+        //List<String> arrList = new ArrayList<>(Arrays.asList(arr);
+
+    }
+
+}
