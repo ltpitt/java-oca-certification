1338edda84ddc0906f3ae2052c4d198bd3a6b1fe
Davide Nastri <d.nastri@gmail.com>
2020-04-11 20:04:46 +0200

    Solved Question 23

---
+++ src/academy/learnprogramming/Questions/Question_23.java
@@ -0,0 +1,22 @@
+package academy.learnprogramming.Questions;
+
+public class Question_23 {
+}
+
+abstract class sPlanet {
+    protected void revolve() {
+    }
+
+    abstract void rotate();
+}
+
+class sEarth extends sPlanet {
+    protected void revolve() {
+
+    }
+
+    public void rotate() {
+
+    }
+
+}
