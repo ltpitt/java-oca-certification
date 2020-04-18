8d95ab02ca6fcbb6e4efa2f662b4ace07068359d
Davide Nastri <d.nastri@gmail.com>
2020-04-18 20:42:18 +0200

    Solved Question 24

---
+++ src/academy/learnprogramming/Questions/Question_24.java
@@ -0,0 +1,36 @@
+package academy.learnprogramming.Questions;
+
+public class Question_24 {
+}
+
+class CheckingAccount {
+    public int amount;
+}
+
+class Geppo {
+    public static void main(String[] args) {
+        CheckingAccount acct = new CheckingAccount();
+//        amount = 100;
+    }
+}
+
+
+class Sports {
+
+    int num_players;
+    String name, ground_condition;
+
+    Sports(int np, String sname, String sground) {
+        num_players = np;
+        name = sname;
+        ground_condition = sground;
+    }
+}
+
+//class  Cricket  extends  Sports  {
+//
+//    int num_umpires;
+//    int num_substitutes;
+//
+//
+//}
