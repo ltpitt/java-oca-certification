8b4539e802116c431bc1052f10cddf98cbb6d4f5
Davide Nastri <d.nastri@gmail.com>
2020-04-21 23:13:26 +0200

    Solved Question 26

---
+++ src/academy/learnprogramming/Questions/Question_26.java
@@ -0,0 +1,10 @@
+package academy.learnprogramming.Questions;
+
+public class Question_26 {
+
+    public static void main(String[] args) {
+        Short s1 = 200;
+        Integer s2 = 400;
+        Long s3 = (long) s1 + s2;
+    }
+}
