a81047495cdb0472972b5a60e7d4abd6cdcc7f5e
Davide Nastri <d.nastri@gmail.com>
2020-04-25 22:13:13 +0200

    Solved Question 28

---
+++ src/academy/learnprogramming/Questions/Question_28.java
@@ -0,0 +1,18 @@
+package academy.learnprogramming.Questions;
+
+public class Question_28 {
+    public static void main(String[] args) {
+        // Option A
+        float flt = 100.00F;
+        // Option B
+        float flt2 = (float) 1_11.00;
+        // Option C --> does not compile
+        //Float flt3 = 100.00;
+        // Option D --> does not compile
+        //double y1 = 203.22;
+        //float flt4 = y1;
+        // Option E
+        int y2 = 100;
+        float flt5 = (float) y2;
+    }
+}
