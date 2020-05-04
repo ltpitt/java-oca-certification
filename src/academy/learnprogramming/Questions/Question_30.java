0a1bbae2a271d3e0df56b942e25246ee04d643f2
Davide Nastri <d.nastri@gmail.com>
2020-05-04 21:37:06 +0200

    Solved Question 30

---
+++ src/academy/learnprogramming/Questions/Question_30.java
@@ -0,0 +1,21 @@
+package academy.learnprogramming.Questions;
+
+public class Question_30 {
+
+    public static void main(String[] args) {
+        Question_30 ex = new Question_30();
+        int cardNo = 12344;
+        // Compilation only fails at line 17 because of Unhandled Exception
+        ex.readCard(cardNo);
+        ex.checkCard(cardNo);
+    }
+
+    void readCard(int cardNo) throws Exception {
+        System.out.println("Reading card");
+    }
+
+    void checkCard(int cardNo) throws RuntimeException {
+        System.out.println("Checking Card");
+    }
+
+}
